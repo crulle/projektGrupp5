ed0a7c14903dc57486ce06a1b707bf1c75235e6e
AppsDaniel <apps.DanielLarsson@gmail.com>
2017-10-04 12:01:17 +0200

    new

--- GitHub/src/Mani.java
+++ GitHub/src/Mani.java
@@ -1,16 +1,42 @@
+import java.util.Scanner;
 
 public class Mani {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hej");
+		Scanner textInput = new Scanner(System.in);
 
-		System.out.println("kurt");
+		System.out.println("Hello and Welcome to Casino group 5!");
+		System.out.println("What's your name sir/miss?");
+		String playerName = textInput.nextLine();
 
-		System.out.println("nehdas");
-		//hej igen
+		Guest Spelare1 = new Guest(playerName, 100);
+		Dealer dealertest = new Dealer();
 		
-		//hej yttligare
+		Spelare1.getHand();
+		System.out.println(dealertest.toString());
+		int dealerSum;
+		int hit = Spelare1.hit();
+		int playerSum = hit;
+		
+		
+		if (playerSum<=21) 
+		{
+			
+			dealerSum = dealertest.dealaersChoice();
+			if (dealerSum >= playerSum && dealerSum <= 21 || playerSum>21) {
+			System.out.println("Dealer Win");
+			}else
+			System.out.println("You win!");
+		}
+		else 
+			System.out.println("\nYou got to much, Dealer Win");
+
+
+		textInput.close();
+		
+		
+
 	}
 
 }
