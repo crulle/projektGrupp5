d8c2d9f7a43768dc87263e38a0968b9a1a4e7818
AppsDaniel <apps.DanielLarsson@gmail.com>
2017-10-04 12:00:38 +0200

    NEW

--- GitHub/src/Hand.java
+++ GitHub/src/Hand.java
@@ -1,4 +1,46 @@
+import java.util.Random;
 
 public class Hand {
-  // hej
-}
+
+	String colour[]= {"heart", "spade", "clubs", "dimond"};
+	String kort[] = {"Ess", "2", "3", "4", "5", "6", "7", "8", "9","10", "Knekt", "Dam", "Kung" };
+	Random random = new Random();
+	
+	 
+	 
+
+	 
+
+
+	String HandValue1() {
+		
+		String kort1 = kort[random.nextInt(kort.length)];
+
+		if (kort1.equals("Knekt") || kort1.equals("Dam") || kort1.equals("Kung"))
+		 {
+		 kort1=kort[9];
+		 }
+		 if (kort1.equals("Ess"))
+		 {
+		 kort1=kort[9];
+		 }
+
+//		 System.out.println(kort101);
+		return kort1;
+	}
+	String HandValue2() {
+		String kort2 = kort[random.nextInt(kort.length)];
+//		String kort202 = colour[random.nextInt(colour.length)] +  " " +kort2;
+		 if (kort2.equals("Knekt") || kort2.equals("Dam") || kort2.equals("Kung"))
+		 {
+		 kort2=kort[9];
+		 }
+		 if (kort2.equals("Ess"))
+		 {
+		 kort2=kort[9];
+		 }
+
+//			System.out.println(kort202);
+		return kort2;
+	}
+}	
