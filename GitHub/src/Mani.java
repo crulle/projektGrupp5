29d8903100ed2cdf35ad9728c65f4f090b6fafe9
sebastiankarlsson <sebastiankarlsson@192.168.20.196>
2017-10-06 10:16:30 +0200

    hej

--- GitHub/src/Mani.java
+++ GitHub/src/Mani.java
@@ -5,45 +5,38 @@ public class Mani {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner textInput = new Scanner(System.in);
-
+		
 		System.out.println("Hello and Welcome to Casino group 5!");
 		System.out.println("What's your name sir/miss?");
 		String playerName = textInput.nextLine();
 
-		System.out.println("Hello " + playerName + " how much money do you have? ");
-		int money = textInput.nextInt();
-
-		Guest spelare = new Guest(playerName, money);
-		Dealer dealer = new Dealer();
-		System.out.println("How much would you like to bet?");
-		int bet = textInput.nextInt();
+		Guest Spelare1 = new Guest(playerName, 100);
+		Dealer dealertest = new Dealer();
 		
-		do {
-		if (bet>money) {
-			System.out.println(playerName+" don't have that much moeny...try again");
-			bet = textInput.nextInt();
-		} else {
-		spelare.getHand();
-		System.out.println(dealer.toString());
-		int playerSum = spelare.hit();
-
-		if (playerSum <= 21) {
-			int dealerSum = dealer.dealaersChoice();
-			if (dealerSum >= playerSum && dealerSum <= 21 || playerSum > 21) {
-				System.out.println("Dealer Win");
-				System.out.println("New Balance: "+(money-bet));
-				break;
-			} else
-				System.out.println("You win!");
-			System.out.println("New Balance: "+(bet+money));
-			break;
-		} else
-			System.out.println("\nYou got to much, Dealer Win");
-		System.out.println("New Balance: "+(money-bet));
-		break;
+		Spelare1.getHand();
+		System.out.println(dealertest.toString());
+		int dealerSum;
+		int hit = Spelare1.hit();
+		int playerSum = hit;
+		
+		
+		if (playerSum<=21) 
+		{
+			
+			dealerSum = dealertest.dealaersChoice();
+			if (dealerSum >= playerSum && dealerSum <= 21 || playerSum>21) {
+			System.out.println("Dealer Win");
+			}else
+			System.out.println("You win!");
 		}
-		}while (bet<=money);
+		else 
+			System.out.println("\nYou got to much, Dealer Win");
+
+
 		textInput.close();
+		
+		
 
 	}
+
 }
